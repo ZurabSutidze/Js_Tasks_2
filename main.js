af447670b029f27851b600ec5330ef1408e91506
ZurabSutidze <zuka.sutidze@gmail.com>
2024-04-18 20:27:02 +0400

    Js_Tasks_3

---
+++ main.js
@@ -0,0 +1,76 @@
+/////////////////////////      1       ///////////////////////
+
+
+// function ObjEntries(obj) {
+//     return Object.entries(obj)
+// }
+
+
+/////////////////////////      2       ///////////////////////
+
+
+// function sameKeys(obj1, obj2) {
+//     const keys1 = Object.keys(obj1);
+//     const keys2 = Object.keys(obj2);
+
+//     for (let key of keys1) {
+//         if (!keys2.includes(key)) {
+//             return false
+//         }
+//     }
+//     return true;
+// }
+
+
+
+/////////////////////////      3      ///////////////////////
+
+
+// function MergeObjs(...objs) {
+//     return Object.assign({} , ...objs)
+// }
+
+
+/////////////////////////      4      ///////////////////////
+
+// function DeleteKey(object, key) {
+//     delete object[`${key}`]
+//     return object
+// }
+
+
+
+/////////////////////////      5     ///////////////////////
+
+// class Car {
+//     constructor(model, year, make) {
+//         this.Carmodel = model
+//         this.Caryear = year
+//         this.Carmake =  make
+//     }
+
+//     DisplayInfo() {
+//         console.log(`Model: ${this.Carmodel} , Year: ${this.Caryear} , Make: ${this.Carmake}`)
+//     }
+// }
+
+/////////////////////////     6    ///////////////////////
+
+// class Rectangle{
+//     constructor(width, height) {
+//         this.Recwidth = width
+//         this.Recheight = height
+//     }
+
+//     getArea(){
+//         return this.Recheight * this.Recwidth
+//     }
+
+//     getPerimeter(){
+//         return 2 * (this.Recheight + this.Recwidth)
+//     }
+
+//     isSquare() {
+//         return this.width === this.height;
+//       }
+// }
